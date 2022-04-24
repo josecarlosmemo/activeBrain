926af9aa0c60d432106f05653c85fcfd676d6a1c
josecarlosmemo <misterio205.1@gmail.com>
2022-04-24 13:33:37 -0500

    Support para varios headers

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,11 +2,20 @@ import { Fragment } from "react";
 
 import classes from "./Layout.module.css";
 import MainNavigation from "./MainNavigation";
+import { useMatch } from "react-router-dom";
+import HomeNavigation from "./HomeNavigation";
 
 const Layout = (props) => {
+  const isHome = useMatch("/home"); // TODO en algun punto esto debe ser si hay inicio de sesión
+  const isPrueba = useMatch("/prueba-rapida");
+  const test = isHome || isPrueba;
+  // console.log(isHome);
+
   return (
     <Fragment>
-      <MainNavigation />
+      {!test && <MainNavigation />}
+      {test && <HomeNavigation />}
+
       <main className={classes.main}>{props.children}</main>
     </Fragment>
   );
