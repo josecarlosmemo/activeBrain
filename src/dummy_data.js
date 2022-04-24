72587f2808ce372a17f88e675de205da1af09af1
josecarlosmemo <misterio205.1@gmail.com>
2022-04-24 13:33:55 -0500

    Más datos

--- src/dummy_data.js
+++ src/dummy_data.js
@@ -1,7 +1,32 @@
 const pacientes = [
-  { id: "0", nombre: "Juan", apellidoP: "Perez", apellidoM: "Hidalgo" },
-  { id: "1", nombre: "Pedro", apellidoP: "Perez", apellidoM: "Hidalgo" },
-  { id: "2", nombre: "Fifo", apellidoP: "Perez", apellidoM: "Hidalgo" },
+  {
+    id: "0",
+    nombre: "Juan",
+    apellidoP: "Perez",
+    apellidoM: "Hidalgo",
+    edad: 69,
+  },
+  {
+    id: "1",
+    nombre: "Pedro",
+    apellidoP: "Morissette",
+    apellidoM: "Schamberger",
+    edad: 82,
+  },
+  {
+    id: "2",
+    nombre: "Fifo",
+    apellidoP: "Kessler",
+    apellidoM: "Ziemann",
+    edad: 99,
+  },
+  {
+    id: "3",
+    nombre: "Jensen",
+    apellidoP: "Gutkowski",
+    apellidoM: "Gorczany",
+    edad: 76,
+  },
 ];
 
 export default pacientes;
