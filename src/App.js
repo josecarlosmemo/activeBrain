e41443de68f556aa359b460c389eb69c4b2f67c2
josecarlosmemo <misterio205.1@gmail.com>
2022-04-24 13:30:00 -0500

    Se agrego route de PruebaRapida

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import PruebaRapida from "./pages/PruebaRapida";
 import Register from "./pages/Register";
 import Welcome from "./pages/Welcome";
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/prueba-rapida" element={<PruebaRapida />} />
       </Routes>
     </Layout>
   );
